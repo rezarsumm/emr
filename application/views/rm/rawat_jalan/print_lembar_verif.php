27e7b9084a118a55a2236dc240ac2256dadef68a
rezarsumm <programerrsumm@gmail.com>
2024-01-28 07:57:54 +0700

    lembar verif perbaikan dan enter pada diagnosa sekunder edit

--- application/views/rm/rawat_jalan/print_lembar_verif.php
+++ application/views/rm/rawat_jalan/print_lembar_verif.php
@@ -227,8 +227,8 @@
                else{
                    echo $rs_resume['FS_DIAG_UTAMA'];}?></td>
             <td style="border-left:solid 1px #000000;border-bottom:solid 1px #000000;text-align:;border-right:solid 1px #000000;border-top:solid 1px #000000;">  </td>
-            <td style="border-left:solid 1px #000000;border-bottom:solid 1px #000000;text-align:;border-right:solid 1px #000000;border-top:solid 1px #000000;"><?php echo $result['Nama_Dokter']?></td>
-             <td style="border-left:solid 1px #000000;border-bottom:solid 1px #000000;text-align:;border-right:solid 1px #000000;border-top:solid 1px #000000;"><qrcode value="<?= $result['Nama_Dokter']; ?> pada <?= date("d-m-Y", strtotime($asal['TANGGAL'])); ?> " ec="H" style="width: 10mm; background-color: white; color: black;"></qrcode></td>
+            <td style="border-left:solid 1px #000000;border-bottom:solid 1px #000000;text-align:;border-right:solid 1px #000000;border-top:solid 1px #000000;"><?php if($result['FS_DIAGNOSA']!=null){ echo $result['Nama_Dokter']; }?></td>
+             <td style="border-left:solid 1px #000000;border-bottom:solid 1px #000000;text-align:;border-right:solid 1px #000000;border-top:solid 1px #000000;"><?php if($result['FS_DIAGNOSA']!=null){ ?><qrcode value="<?= $result['Nama_Dokter']; ?> pada <?= date("d-m-Y", strtotime($asal['TANGGAL'])); ?> " ec="H" style="width: 10mm; background-color: white; color: black;"></qrcode> <?php } ?></td>
         </tr>
         </tbody>
     </table>
@@ -266,8 +266,10 @@
                else{
                    echo '';}?></td>
                     <td style="border-left:solid 1px #000000;border-bottom:solid 1px #000000;text-align:;border-right:solid 1px #000000;border-top:solid 1px #000000;">  </td>
-                    <td style="border-left:solid 1px #000000;border-bottom:solid 1px #000000;text-align:;border-right:solid 1px #000000;border-top:solid 1px #000000;"><?php echo $result['Nama_Dokter']?></td>
-                    <td style="border-left:solid 1px #000000;border-bottom:solid 1px #000000;text-align:;border-right:solid 1px #000000;border-top:solid 1px #000000;"><qrcode value="<?= $result['Nama_Dokter']; ?> pada <?= date("d-m-Y", strtotime($asal['TANGGAL'])); ?> " ec="H" style="width: 10mm; background-color: white; color: black;"></qrcode></td>
+                    <td style="border-left:solid 1px #000000;border-bottom:solid 1px #000000;text-align:;border-right:solid 1px #000000;border-top:solid 1px #000000;"><?php if($result['FS_DIAGNOSA_SEKUNDER']!=null){
+                            echo $result['Nama_Dokter'];
+                    } ?></td>
+                    <td style="border-left:solid 1px #000000;border-bottom:solid 1px #000000;text-align:;border-right:solid 1px #000000;border-top:solid 1px #000000;"><?php if($result['FS_DIAGNOSA_SEKUNDER']!=null){ ?><qrcode value="<?= $result['Nama_Dokter']; ?> pada <?= date("d-m-Y", strtotime($asal['TANGGAL'])); ?> " ec="H" style="width: 10mm; background-color: white; color: black;"></qrcode> <?php } ?></td>
                 </tr>
                 <tr>
                     <td style="height:7px; border-left:solid 1px white;border-bottom:solid 1px #000000;text-align:;border-right:solid 1px #000000;border-top:solid 1px #000000;"></td>
